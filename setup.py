e97fcd496d783cabd9bbfc60c474b3d9d5763dae
arham <arhamop937@gmail.com>
2026-01-01 20:33:33 +0500

    local package setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ecommbot",
+    version="0.0.1",
+    author="Arham Rafique",
+    author_email="arhammrafique@gmail.com",
+    packages=find_packages(),
+    install_requires=['langchain-astradb','langchain ','langchain-openai','datasets','pypdf','python-dotenv','flask']
+)
